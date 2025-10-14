5ae3252b15c1788c8d7722f734c278d9cc443f41
atouisassi <atouisassi@gmail.com>
2025-10-14 11:10:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const urlsToCache = [
   "./index.html",
   "./style.css",
   "./script.js", // si séparé
+  "./xlsx.full.min.js",
   "https://cdn.jsdelivr.net/npm/xlsx@0.20.2/dist/xlsx.full.min.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
@@ -11,7 +12,7 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
-      const urls = ['/', '/style.css', '/script.js', '/icons/icon-192.png', '/manifest.json'];
+      const urls = ['/', '/style.css', '/script.js','./xlsx.full.min.js', '/icons/icon-192.png', '/manifest.json'];
       return Promise.all(
         urls.map(url => cache.add(url).catch(err => {
           console.warn('Fichier non trouvé, cache ignoré:', url);
