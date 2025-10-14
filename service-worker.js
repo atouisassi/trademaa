377866851e168e5819404d3d63742d1138e796fb
atouisassi <atouisassi@gmail.com>
2025-10-14 14:12:46 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
 const CACHE='trademaa';
-const ASSETS=['./','./index.html','./style.css','./script.js','./xlsx.full.min.js','./manifest.json'];
+const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
